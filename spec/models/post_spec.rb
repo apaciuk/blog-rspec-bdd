c4b066c6b45f34502c19d6ad82d09e970f03b1bb
Paul Anthony McGowan <xhostcom@gmail.com>
2022-05-10 07:58:55 +0100

    2 pass

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,7 +1,18 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
   current_user = User.first_or_create!(email: 'paul@example.com', password: 'password', password_confirmation: 'password')
-  let(:post) { Post.new(title: 'My first post', content: 'This is my first post', user: current_user) }
-
+  let(:post) do
+    Post.new(title: 'Has A Valid Title', body: 'This is a valid first post', views: 0, user: current_user)
+  end
+  it 'has a title' do
+    expect(post).to be_valid
+  end
+  it 'has a body between 5 and 100 characters' do
+    post.body = '12345'
+    expect(post).to be_valid
+  end
 end
+
